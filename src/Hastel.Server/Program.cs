51708772b692792e0b87cacb0f1342ea0d8a5e00
JTnadrooi <jtnadrooi@gmail.com>
2026-02-13 18:22:40 +0100

    Code cleanup

--- src/Hastel.Server/Program.cs
+++ src/Hastel.Server/Program.cs
@@ -1,8 +1,10 @@
-﻿using AsitLib.CommandLine;
+﻿using AsitLib;
+using AsitLib.CommandLine;
 using AsitLib.Diagnostics;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
@@ -12,6 +14,8 @@ namespace Hastel.Server
 {
     public static class Program
     {
+        private const string Prefix = "http://localhost:5000/";
+
         public static void Main(string[] args)
         {
             using HttpListener listener = new HttpListener();
@@ -19,11 +23,14 @@ namespace Hastel.Server
             CommandEngine engine = new CommandEngine();
             engine.Populate();
 
-            listener.Prefixes.Add("http://localhost:5000/");
+            listener.Prefixes.Add(Prefix);
             listener.Start();
 
-            logger.Log("server running at http://localhost:5000/");
+            logger.Log($"server running at {Prefix}");
+            logger.Log($"found commands: '{engine.Commands.Select(c => c.Key).ToJoinedString(", ")}'");
             logger.Log("press Ctrl+C to stop.");
+
+            logger.Log(">listening...");
 
             while (true)
             {
