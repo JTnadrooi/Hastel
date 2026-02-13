b661f30261e8207a13cc30cc7e54adc502c62d1b
JTnadrooi <jtnadrooi@gmail.com>
2026-02-13 10:50:08 +0100

    Add MSTest csproj

---
+++ src/Hastel.Server.Tests/Test1.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace Hastel.Server.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
