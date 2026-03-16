eab34c6d1c7fe02baaeaa5da28f3de07cf4bdcc0
JTnadrooi <jtnadrooi@gmail.com>
2026-03-16 13:03:41 +0100

    Add CommandWebResponse.Succes static property

--- src/Hastel.Server/CommandWebResponse.cs
+++ src/Hastel.Server/CommandWebResponse.cs
@@ -6,11 +6,21 @@ namespace Hastel.Server
     public class CommandWebResponse
     {
         public HttpStatusCode StatusCode { get; init; } = HttpStatusCode.OK;
+        /// <summary>
+        /// The request body. Does not automatically get converted to JSON.
+        /// </summary>
         public string String { get; init; } = string.Empty;
         public Cookie[] Cookies { get; init; } = Array.Empty<Cookie>();
 
-        public static CommandWebResponse FromStatusCode(HttpStatusCode statusCode)
-            => new CommandWebResponse() { StatusCode = statusCode };
+        /// <summary>
+        /// <code>
+        /// FromStatusCode(HttpStatusCode.OK);
+        /// </code>
+        /// </summary>
+        public static CommandWebResponse Succes => FromStatusCode(HttpStatusCode.OK);
+
+        public static CommandWebResponse FromStatusCode(HttpStatusCode statusCode, string msg = "")
+            => new CommandWebResponse() { StatusCode = statusCode, String = msg };
 
         public static CommandWebResponse FromString(string str)
             => new CommandWebResponse() { String = str };
@@ -26,7 +36,7 @@ namespace Hastel.Server
                 },
                 _ => new CommandWebResponse()
                 {
-                    StatusCode = HttpStatusCode.InternalServerError,
+                    StatusCode = HttpStatusCode.InternalServerError, // normal exceptions might hold sensitive data.
                 },
             };
         }
